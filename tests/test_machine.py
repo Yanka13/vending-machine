1b5f5bab3b7fccc26ca20d3237477b407682f4f3
Yannis Bouhamou <61419311+Yanka13@users.noreply.github.com>
2021-12-20 16:13:08 +0100

    improve documentation

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -7,51 +7,55 @@ class MachineTest(unittest.TestCase):
     #test that an agent can refill the machine
     def test_agent_can_refill_A(self):
 
-        # Instanciate minimal rack and machine.
+        # Instanciate minimal rack and machine set up
         racks = [Rack("A", "", 100)]
-        machine = Machine(racks)
+        machine = Machine(racks) #Machine that accepts "A" product
 
-        # I need to refill this machine with 10 biscuits "A"
+        # Machine refilled with 10 biscuits "A"
         machine.refill("A", 10)
 
-        # Assert that I have 10  biscuits "A" in my machine
+        # Assert that there is  10  biscuits "A" in the machine
         self.assertEqual(machine.racks["A"].quantity , 10)
 
     #User can buy an article
     def test_user_can_buy_article_A(self):
 
-        # instanciate minimum machine
+        # instanciate minimum machine set up with empty stock
         racks = [Rack("A", "", 100)]
 
         machine = Machine(racks, 0)
 
         machine.refill("A", 1)
 
-        # user insert money
+        # user insert one dollar
         machine.insert(Coin.DOLLAR)
 
-        #user choose product A
+        #user orders product A
         outcome = machine.press("A")
 
         # assertions
         self.assertTrue(outcome)
         self.assertEqual(machine.racks["A"].quantity, 0)
         self.assertEqual(machine.coins[Coin.DOLLAR], 1)
+        self.assertEqual(machine.amount, 0)
+
 
     #User can get his change back
     def test_user_can_get_change_back(self):
 
-        # instanciate machine (this time with 10 coins each)
+        # instanciate machine, this time with 10 coins per category
         racks = [Rack("C", "", 85)]
-        machine = Machine(racks, 10)
+        machine = Machine(racks, 10) # machine that accepts bottle of water
+
+        # Add one bottle of water to the machine
         machine.refill("C", 1)
 
-        # user insert money
+        # user insert one dollar
         machine.insert(Coin.DOLLAR)
 
-        #user choose product A
+        #user orders one bottle of water
         outcome = machine.press("C")
 
-        #Assert that there are 15 cents left
+        #Assert that the machine "gives back 15 cents to the users"
         self.assertEqual(machine.coins[Coin.DIME], 9)
         self.assertEqual(machine.coins[Coin.NICKEL], 9)
